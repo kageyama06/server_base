2af083e51d35ae1db0589ac0ea7fb397fbb11a1e
Thiago <thiago_r_araujo@estudante.sesisenai.or.br>
2023-04-05 11:58:24 -0300

    primeira versão do server base

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+const hostname = 'localhost'
+const PORT = 3000
+
+// configurações do express //
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+// ----------------------- //
+
+// arquivos estáticos //
+app.use(express.static('public'))
+// ------------------ //
+
+// Rotas do projeto //
+app.get('../src/', (req,res)=>{
+    res.render('contato.html')
+})
+
+app.get('/', (req,res)=>{
+    res.render('index.html')
+})
+// ---------------- //
+
+app.listen(PORT,hostname, ()=>{
+    console.log(`O servidor está rodando ${hostname}:${PORT}`)
+})
